72e6e135a8023f94a9dc2ca53ea5b9511a4ad75a
AidenValley <jhyjsh953@gmail.com>
2022-10-22 12:05:12 -0700

    feat:simplecodes added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello Tanner');
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Post = require('./schemas/post');
+const User = require('./schemas/user');
+const { CommentModel } = require('./schemas/comment');
+
