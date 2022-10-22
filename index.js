72af8c902fd9f771a85658b32dba4cfd3984f55c
Aiden Jang <107300143+AidenValley@users.noreply.github.com>
2022-10-22 12:07:38 -0700

    feat:simplecodes added (#6)
    Co-authored-by: Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-
 console.log('Hello Tanner');
-
-
 console.log('this is David');
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Post = require('./schemas/post');
+const User = require('./schemas/user');
+const { CommentModel } = require('./schemas/comment');
+
+
 
 
